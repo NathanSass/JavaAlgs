f61a07159b505daba4de6da4b194acb6fbc8bfeb
Nathan Sass <nsass711@gmail.com>
2016-09-13 11:21:16 -0700

    working solution for all test cases

--- src/HiCal.java
+++ src/HiCal.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  *   [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
@@ -7,67 +7,44 @@ import java.util.ArrayList;
 public class HiCal {
     public static void main(String[] args) {
         ArrayList<Meeting> meetings = new ArrayList<>();
-        meetings.add(new Meeting(0,2));
-        meetings.add(new Meeting(1,4));
-        meetings.add(new Meeting(7,10));
-        meetings.add(new Meeting(6,8));
-        meetings.add(new Meeting(1,5));
-//        meetings.add(new Meeting(2,3));
+        meetings.add(new Meeting(1,3));
+        meetings.add(new Meeting(2,4));
 
-
-        System.out.println(new HiCal().consolidate(meetings).toString());
+        System.out.println(new HiCal().consolidate2(meetings).toString());
 
     }
 
-
-    public ArrayList<Meeting> consolidate(ArrayList<Meeting> meetingsArr) {
-        ArrayList<Meeting> meetings = meetingsArr;
-
-        ArrayList<Meeting> consolidatedMeetings = new ArrayList<>();
-        ArrayList<Meeting> discaredMeetings = new ArrayList<>();
-
-        for(int i = 0; i < meetings.size(); i++) {
-            Meeting currentMeeting = meetings.get(i);
-            // Check through existing start values to see if current start value is greater than an
-            // existing start value and less than the existing end value
-                // if true: Get the max and make it the new end value
-                    // Then do the check for this object on the array
-            for (int x = 0; x < meetings.size(); x++) {
-                if(i == x) {continue; }
-
-                Meeting meeting = meetings.get(x);
-                if ( (meeting.startTime >= currentMeeting.startTime) &&
-                        (meeting.startTime <= currentMeeting.endTime) ) { // will have overlap
-
-                    discaredMeetings.add(currentMeeting);
-                    discaredMeetings.add(meeting);
-
-                    Meeting combinedMeeting = new Meeting();
-                    combinedMeeting.startTime = Math.min(currentMeeting.startTime, meeting.startTime);
-                    combinedMeeting.endTime = Math.max(currentMeeting.endTime, meeting.endTime);
-                    consolidatedMeetings.add(i, combinedMeeting);
-                } else {
-                    if (!discaredMeetings.contains(currentMeeting) && !consolidatedMeetings.contains(currentMeeting)) {
-                        consolidatedMeetings.add(currentMeeting);
-                    }
-                }
+    public ArrayList<Meeting> consolidate2(ArrayList<Meeting> meetingsArr) {
+        Collections.sort(meetingsArr, new Comparator<Meeting>() {
+            @Override
+            public int compare(Meeting o1, Meeting o2) {
+                return o1.startTime - o2.startTime;
             }
+        });
 
-        }
+        int i = 0;
+        while (i < meetingsArr.size() - 1) {
+            Meeting meeting1 = meetingsArr.get(i);
+            Meeting meeting2 = meetingsArr.get(i + 1);
 
-        for (Meeting discarded : discaredMeetings) {
-            if (consolidatedMeetings.contains(discarded)) {
-                consolidatedMeetings.remove(discarded);
+            if (meeting2.startTime <= meeting1.endTime) {
+                // Do merge magic
+                meetingsArr.remove(meeting1);
+                meetingsArr.remove(meeting2);
+
+                Meeting newMeeting = new Meeting();
+                newMeeting.startTime = meeting1.startTime;
+                newMeeting.endTime = Math.max(meeting1.endTime, meeting2.endTime);
+                meetingsArr.add(i, newMeeting);
+            } else {
+                i+= 1;
             }
         }
-
-
-
-        return consolidatedMeetings;
-
+        return meetingsArr;
     }
 
-    public static class Meeting {
+
+    public static class Meeting{
 
         int startTime;
         int endTime;
